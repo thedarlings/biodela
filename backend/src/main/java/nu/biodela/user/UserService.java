926da11aafb909d487d8d17145f11d6dab484093
David Tennander <david.tennander@gmail.com>
2018-03-17 23:39:07 +0100

    Adds authentication to user module.

--- backend/src/main/java/nu/biodela/user/UserService.java
+++ backend/src/main/java/nu/biodela/user/UserService.java
@@ -5,27 +5,29 @@ import static io.javalin.ApiBuilder.get;
 import static io.javalin.ApiBuilder.patch;
 import static io.javalin.ApiBuilder.path;
 import static io.javalin.ApiBuilder.post;
+import static io.javalin.security.Role.roles;
 
 import javax.inject.Inject;
 import nu.biodela.Service;
+import nu.biodela.authentication.ApiRole;
 
 public class UserService implements Service {
   private final UserController userController;
 
   @Inject
-  public UserService(UserController userController) {
+  UserService(UserController userController) {
     this.userController = userController;
   }
 
   @Override
   public void setUpRoutes() {
     path("users", () -> {
-      get(userController::getAllUsers);
-      post(userController::createUser);
+      get(userController::getAllUsers, roles(ApiRole.USER));
+      post(userController::createUser, roles(ApiRole.ANYONE));
       path(":id", () -> {
-        get(userController::getUser);
-        patch(userController::updateUser);
-        delete(userController::deleteUser);
+        get(userController::getUser, roles(ApiRole.USER));
+        patch(userController::updateUser, roles(ApiRole.USER));
+        delete(userController::deleteUser, roles(ApiRole.USER));
       });
     });
   }
