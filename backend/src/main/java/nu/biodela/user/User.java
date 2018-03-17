3fa68525d2650688a7934b7ef77a2df512ea2bfe
rebecca <rebecca@kry.se>
2018-03-17 17:34:09 +0100

    Add User class

---
+++ backend/src/main/java/nu/biodela/user/User.java
@@ -0,0 +1,50 @@
+package nu.biodela.user;
+
+import java.util.UUID;
+
+public class User {
+
+  private UUID id;
+  private String username;
+  private String password;
+  private String email;
+
+  User(String username, String password, String email) {
+    this.id = UUID.randomUUID();
+    this.username = username;
+    this.password = password;
+    this.email = email;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
