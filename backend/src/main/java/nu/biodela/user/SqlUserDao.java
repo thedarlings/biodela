bf5585727b10516864a6a6eb99ed41c88ca94b93
rebecca <rebecca@kry.se>
2018-03-18 09:02:19 +0100

    refactor

--- backend/src/main/java/nu/biodela/user/SqlUserDao.java
+++ backend/src/main/java/nu/biodela/user/SqlUserDao.java
@@ -26,6 +26,75 @@ public class SqlUserDao implements UserDao {
     this.logger = LoggerFactory.getLogger(SqlUserDao.class);
   }
 
+
+  @Override
+  public List<User> findAll() {
+
+    Connection dbConnection = null;
+    Statement statement = null;
+
+    String selectTableSQL = "SELECT * from users";
+
+    return getUsers(dbConnection, statement, selectTableSQL);
+
+  }
+
+  @Override
+  public List<User> findById(String id) {
+    Connection dbConnection = null;
+    Statement statement = null;
+
+    String selectTableSQL = String.format("SELECT * from users WHERE %s=%s", USER_ID, id);
+
+    return getUsers(dbConnection, statement, selectTableSQL);
+  }
+
+  @Override
+  public List<User> findByUsername(String username) {
+    Connection dbConnection = null;
+    Statement statement = null;
+
+    String selectTableSQL = String.format("SELECT * from users WHERE %s=%s", USERNAME, username);
+
+    return getUsers(dbConnection, statement, selectTableSQL);
+  }
+
+  @Override
+  public boolean insertEmployee(User user) {
+    return false;
+  }
+
+  @Override
+  public boolean updateEmployee(User user) {
+    return false;
+  }
+
+  @Override
+  public boolean deleteEmployee(User user) {
+    return false;
+  }
+
+  private List<User> getUsers(Connection dbConnection, Statement statement, String selectTableSQL) {
+    try {
+      dbConnection = dbService.connect();
+      statement = dbConnection.createStatement();
+      logger.info("SQL QUERY TEST: " + selectTableSQL);
+
+      // execute select SQL stetement
+      try (ResultSet rs = statement.executeQuery(selectTableSQL)) {
+        return userResultSetToList(rs);
+      }
+
+    } catch (SQLException e) {
+      logger.error(e.getMessage(), e);
+      throw new RuntimeException(e);
+
+    } finally {
+      closeStatement(statement);
+      closeDbConnection(dbConnection);
+    }
+  }
+
   private List<User> userResultSetToList(ResultSet rs) throws SQLException {
     List<User> allUsers = new ArrayList<>();
     while (rs.next()) {
@@ -61,100 +130,4 @@ public class SqlUserDao implements UserDao {
     }
   }
 
-  @Override
-  public List<User> findAll() {
-
-    Connection dbConnection = null;
-    Statement statement = null;
-
-    String selectTableSQL = "SELECT * from users";
-    try {
-      dbConnection = dbService.connect();
-      statement = dbConnection.createStatement();
-      logger.info("SQL QUERY TEST: " + selectTableSQL);
-
-      // execute select SQL stetement
-      try (ResultSet rs = statement.executeQuery(selectTableSQL)) {
-        return userResultSetToList(rs);
-      }
-
-    } catch (SQLException e) {
-      logger.error(e.getMessage(), e);
-      throw new RuntimeException(e);
-
-    } finally {
-      closeStatement(statement);
-      closeDbConnection(dbConnection);
-    }
-
-  }
-
-  @Override
-  public List<User> findById(String id) {
-    Connection dbConnection = null;
-    Statement statement = null;
-
-    String selectTableSQL = String.format("SELECT * from users WHERE %s=%s", USER_ID, id);
-
-    try {
-      dbConnection = dbService.connect();
-      statement = dbConnection.createStatement();
-      logger.info("SQL QUERY TEST: " + selectTableSQL);
-
-      // execute select SQL stetement
-      try (ResultSet rs = statement.executeQuery(selectTableSQL)) {
-        return userResultSetToList(rs);
-      }
-
-    } catch (SQLException e) {
-      logger.error(e.getMessage(), e);
-      throw new RuntimeException(e);
-
-    } finally {
-      closeStatement(statement);
-      closeDbConnection(dbConnection);
-    }
-  }
-
-  @Override
-  public List<User> findByUsername(String username) {
-    Connection dbConnection = null;
-    Statement statement = null;
-
-    String selectTableSQL = String.format("SELECT * from users WHERE %s=%s", USERNAME, username);
-
-    try {
-      dbConnection = dbService.connect();
-      statement = dbConnection.createStatement();
-      logger.info("SQL QUERY TEST: " + selectTableSQL);
-
-      // execute select SQL stetement
-      try (ResultSet rs = statement.executeQuery(selectTableSQL)) {
-        return userResultSetToList(rs);
-      }
-
-    } catch (SQLException e) {
-      logger.error(e.getMessage(), e);
-      throw new RuntimeException(e);
-
-    } finally {
-      closeStatement(statement);
-      closeDbConnection(dbConnection);
-    }
-  }
-
-  @Override
-  public boolean insertEmployee(User user) {
-    return false;
-  }
-
-  @Override
-  public boolean updateEmployee(User user) {
-    return false;
-  }
-
-  @Override
-  public boolean deleteEmployee(User user) {
-    return false;
-  }
 }
